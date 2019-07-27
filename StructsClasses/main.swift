d87e8363b815002645f7e67837aa96f2193ce49a
Michael Miles <michael.gage.miles@gmail.com>
2019-07-27 15:10:42 -0700

    Initial Commit

---
+++ StructsClasses/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StructsClasses
+//
+//  Created by Michael Miles on 7/27/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
